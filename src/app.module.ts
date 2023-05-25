d0b6716900d14ebf9efbdeac92e38522cda48b57
midasworld <midas117@knou.ac.kr>
2023-05-26 05:19:59 +0900

    feat: `@nextjs/config` 추가 & 이메일 Config 정보 설정 - `@nextjs/config` 의존성 추가 - Joi 의존성 추가 및 설정정보 validation 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
+import { validationSchema } from './config/validation.schema';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import emailConfig from './config/email.config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    UsersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
