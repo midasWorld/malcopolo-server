5bb3d54938e67a9bc5870b5eaa008e5ed4f6e2f5
midasworld <midas117@knou.ac.kr>
2023-05-26 16:35:39 +0900

    feat: 토큰 검증 부분 → AuthGuard 로 분리

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,23 @@
-import { AuthService } from 'src/auth/auth.service';
 import {
   Body,
   Controller,
   Get,
-  Headers,
   Param,
   ParseIntPipe,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from 'src/common/guard/auth.gurad';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UsersService } from './users.service';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserDto } from './dto/user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Post()
   async create(@Body() request: CreateUserDto): Promise<void> {
@@ -37,15 +34,9 @@ export class UsersController {
     return await this.usersService.login(request);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/:id')
-  async getUserInfo(
-    @Headers() headers: any,
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<UserDto> {
-    const token = headers.authorization.split('Bearer ')[1];
-
-    this.authService.verifyToken(token);
-
+  async getUserInfo(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
     return await this.usersService.getUserInfo(id);
   }
 }
