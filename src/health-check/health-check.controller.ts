dfa9d22affbb19b82ac5c32f5cb802a3e930431a
midasworld <midas117@knou.ac.kr>
2023-05-26 22:59:51 +0900

    feat: Health-Check - Prisma Indicator 추가

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -4,12 +4,14 @@ import {
   HealthCheckService,
   HttpHealthIndicator,
 } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from './prisma-health.indicator';
 
 @Controller('health-check')
 export class HealthCheckController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private db: PrismaHealthIndicator,
   ) {}
 
   @Get()
@@ -17,6 +19,7 @@ export class HealthCheckController {
   healthCheck() {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.db.pingCheck('database'),
     ]);
   }
 }
