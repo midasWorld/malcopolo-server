a622a34037aa795bb0b824e8148d2825f693b681
midasworld <midas117@knou.ac.kr>
2023-05-28 06:02:16 +0900

    feat: API prefix 지정 (`/api`)

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { HttpExceptionFilter } from './common/filter/http.exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/api');
+
   app.use(helmet());
   app.use(morgan('tiny'));
   app.use(csrfCheck);
@@ -27,6 +29,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
